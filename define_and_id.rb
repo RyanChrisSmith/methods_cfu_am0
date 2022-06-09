5c51f73c445b62ef2f03e95bff5fdf45f7b91b2f
Ryan Smith <ryan.chris.smith@gmail.com>
2022-06-09 14:29:18 -0600

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -11,17 +11,24 @@
 
 
 "Hello World".include?("Hello")
-
+# The question mark include method is called on string object "Hello World"
+# Include is checking to see if the argument "Hello" is included in the string object "Hello World"
+# The return values is true
 
 "Hello World".end_with?("Hello")
-
+# The question mark end with method is called on string object "Hello World"
+# end with is checking to see if the argument "Hello" is at the end of the string object "Hello World"
+# The return values is false
 
 "Hello World".end_with?("rld")
-
+# The question mark end with method is called on string object "Hello World"
+# end with is checking to see if the argument "rld" is at the end of the string object "Hello World"
+# The return values is true
 
 12.even?
-
+# The question mark even method is called on integer 12. Even is checking if the integer is even.
+# The return value is true
 
 18.next
-
-
+# The next method is called on integer 18. Next is moving the integer to the next whole number.
+# The return value is 19
